2f65f56e1356b2e252dc72f0295b28e47fa78f8f
MesutSoyumert <msoyumert@hotmail.com>
2021-12-10 21:16:33 +0300

    7. ders sonu

--- Modern C# Kursu  .NET Dünyası İçin Sıfırdan Profesyonelliğe_20211208/CCsharpIntro/Project1/Form1.Designer.cs
+++ Modern C# Kursu  .NET Dünyası İçin Sıfırdan Profesyonelliğe_20211208/CCsharpIntro/Project1/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbxProducts = new System.Windows.Forms.ListBox();
+            this.lblProducts = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbxProducts
+            // 
+            this.lbxProducts.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbxProducts.FormattingEnabled = true;
+            this.lbxProducts.ItemHeight = 21;
+            this.lbxProducts.Location = new System.Drawing.Point(51, 187);
+            this.lbxProducts.Name = "lbxProducts";
+            this.lbxProducts.Size = new System.Drawing.Size(152, 235);
+            this.lbxProducts.TabIndex = 0;
+            // 
+            // lblProducts
+            // 
+            this.lblProducts.AutoSize = true;
+            this.lblProducts.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblProducts.Location = new System.Drawing.Point(50, 125);
+            this.lblProducts.Name = "lblProducts";
+            this.lblProducts.Size = new System.Drawing.Size(0, 21);
+            this.lblProducts.TabIndex = 1;
+            this.lblProducts.Click += new System.EventHandler(this.lblProducts_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(135, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 21);
+            this.label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblProducts);
+            this.Controls.Add(this.lbxProducts);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private ListBox lbxProducts;
+        private Label lblProducts;
+        private Label label1;
     }
 }
--- Modern C# Kursu  .NET Dünyası İçin Sıfırdan Profesyonelliğe_20211208/CCsharpIntro/Project1/Form1.cs
+++ Modern C# Kursu  .NET Dünyası İçin Sıfırdan Profesyonelliğe_20211208/CCsharpIntro/Project1/Form1.cs
@@ -6,5 +6,18 @@ namespace Project1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblProducts.Text = "Ürünler";
+            lbxProducts.Items.Add("Laptop");
+            lbxProducts.Items.Add("Masaüstü PC");
+            lbxProducts.Items.Add("Klavye");
+        }
+
+        private void lblProducts_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
