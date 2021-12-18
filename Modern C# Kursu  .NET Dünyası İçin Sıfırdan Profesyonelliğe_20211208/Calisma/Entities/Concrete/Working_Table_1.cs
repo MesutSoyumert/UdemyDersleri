b1de089b37ed35cae27b7fa4be4e56d56ba3d09a
MesutSoyumert <msoyumert@hotmail.com>
2021-12-18 19:39:13 +0300

    Bölüm 4 Ders7 sonu

---
+++ Modern C# Kursu  .NET Dünyası İçin Sıfırdan Profesyonelliğe_20211208/Calisma/Entities/Concrete/Working_Table_1.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Working_Table_1 : IEntity
+    {
+    }
+}
